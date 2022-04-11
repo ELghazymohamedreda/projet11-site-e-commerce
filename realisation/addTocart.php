05bc3867a43d0af510956507f65f8e975be92ccd
ELGHAZY2007 <elghazy.mohamedreda@gmail.com>
2022-04-11 09:58:55 +0000

    up

---
+++ realisation/addTocart.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+include 'cartManager.php';
+$cartManager = new CartManager();
+
+
+
+
+
+
+$id=$_POST['id'];
+
+
+$data = $cartManager->afficherProduit($id);
+
+foreach($data as $value);
+
+
+$valeurs = array(
+    "nom" => $value->getNom(),
+    'prix' => $value->getPrix(),
+    'quantite' => $_POST["quantite"] ,
+    'id' => $value->getId(),
+);
+$cartManager->set($_POST["id"], $valeurs);
+
+
+header("location: panier.php");
